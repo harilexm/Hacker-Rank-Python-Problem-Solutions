f1da41822c9cc55511bacbc49dcd345bda0228de
Abedano <umer.abdullah3241@gmail.com>
2025-08-12 11:09:10 +0500

    Create 5. Loops.py

---
+++ Easy/5. Loops.py
@@ -0,0 +1,15 @@
+# Solution: 1
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(n):
+        print (i**2)
+
+# Solution: 2
+if __name__ == '__main__':
+    n = int(input())
+    
+    a = 0
+    while a < n:
+        print (a**2)
+        a += 1
