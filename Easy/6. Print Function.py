d249af5085ffbd689c8e0ae8e8fcec7a674fac4e
Abedano <umer.abdullah3241@gmail.com>
2025-08-12 11:21:23 +0500

    Create 6. Print Function.py

---
+++ Easy/6. Print Function.py
@@ -0,0 +1,14 @@
+# Solution 1
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(1, n+1):
+        print (i, end='')
+
+# Solution 2
+if __name__ == '__main__':
+    n = int(input())
+    i = 0
+    while i < n:
+        i = i+1
+        print (i, end='')
