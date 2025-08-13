f0c59a2b986a7804ca1705ad69327620cc7a3ef4
Abedano <umer.abdullah3241@gmail.com>
2025-08-13 18:00:19 +0500

    Create 012. Tuples.py

---
+++ Easy/012. Tuples.py
@@ -0,0 +1,7 @@
+# Must change the version to Python 2
+if __name__ == '__main__':
+    n = int(raw_input())
+    integer_list = map(int, raw_input().split())
+    
+    t = tuple(integer_list)
+    print (hash(t))
