89b9f36d8b926fb000c6923c4d8992f7ee858995
Abedano <umer.abdullah3241@gmail.com>
2025-08-13 17:36:30 +0500

    Update 011. Lists.py

--- Easy/011. Lists.py
+++ Easy/011. Lists.py
@@ -1,1 +1,21 @@
-
+if __name__ == '__main__':
+    N = int(input())
+    A = []
+    
+    for _ in range(N):
+        user_input = input().split()
+        
+        if user_input[0] == 'insert':
+            A.insert(int(user_input[1]), int(user_input[2]))
+        elif user_input[0] == 'print':
+            print(A)
+        elif user_input[0] == 'remove':
+            A.remove(int(user_input[1]))
+        elif user_input[0] == 'append':
+            A.append(int(user_input[1]))
+        elif user_input[0] == 'sort':
+            A.sort()
+        elif user_input[0] == 'pop':
+            A.pop()
+        elif user_input[0] == 'reverse':
+            A.reverse()
