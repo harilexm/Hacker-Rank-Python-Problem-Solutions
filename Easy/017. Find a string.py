7491c178c752de9da68be798335bfda8deff2857
Abedano <umer.abdullah3241@gmail.com>
2025-08-16 22:05:57 +0500

    Create 017. Find a string.py

---
+++ Easy/017. Find a string.py
@@ -0,0 +1,19 @@
+def count_substring(string, sub_string):
+    start = 0
+    counter = 0
+    end = len(sub_string)
+
+    while end <= len(string):
+        if sub_string == string[start: end]:
+            counter += 1
+        start += 1
+        end += 1
+    
+    return counter
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
