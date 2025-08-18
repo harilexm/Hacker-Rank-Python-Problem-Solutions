677dc2f8f129ed691812fbb3f773e72458b0ac04
Abedano <umer.abdullah3241@gmail.com>
2025-08-18 22:42:14 +0500

    Create 022. String Formatting.py

---
+++ Easy/022. String Formatting.py
@@ -0,0 +1,11 @@
+# Solution: 1
+def print_formatted(number):
+    # your code goes here
+    pad = number.bit_length()
+    for i in range(1, number + 1):
+        print (f'{i:{pad}d} {i:{pad}o} {i:{pad}X} {i:{pad}b}' )
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
+
