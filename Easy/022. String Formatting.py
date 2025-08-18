a273196120d44765d56afb2335b9aa870e7f3ab7
Abedano <umer.abdullah3241@gmail.com>
2025-08-18 23:12:43 +0500

    Update 022. String Formatting.py

--- Easy/022. String Formatting.py
+++ Easy/022. String Formatting.py
@@ -9,3 +9,17 @@ if __name__ == '__main__':
     n = int(input())
     print_formatted(n)
 
+# Solution: 2
+def print_formatted(number):
+    # your code goes here
+    pad = number.bit_length()
+    for i in range(1, number + 1):
+        deci = str(i)
+        octa = oct(i)[2:]
+        hexa = hex(i)[2:].upper()
+        bina = bin(i)[2:]
+        print (deci.rjust(pad), octa.rjust(pad), hexa.rjust(pad), bina.rjust(pad))
+    
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
