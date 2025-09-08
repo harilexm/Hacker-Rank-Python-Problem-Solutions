b6633fd51947f418bc97327006a5cba90f6f706d
Abedano <umer.abdullah3241@gmail.com>
2025-09-08 15:09:10 +0500

    Create 050. Check Subset.py

---
+++ Easy/050. Check Subset.py
@@ -0,0 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+T = int(input())
+
+for i in range(T):
+    a = int(input())
+    A = set(map(int, input().split()))
+    b = int(input())
+    B = set(map(int, input().split()))
+    
+    if A.issubset(B):
+        print(True)
+    else:
+        print(False)
