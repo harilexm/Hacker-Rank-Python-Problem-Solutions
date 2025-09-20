1e1e4f31ce1eb2981a99534e36ab3139dd41f75b
harilexm <umer.abdullah9891@gmail.com>
2025-09-20 23:24:49 +0500

    final working code

--- Easy/056. Any or All.py
+++ Easy/056. Any or All.py
@@ -1,2 +1,3 @@
-N = int(input())
-n = list(map(int, input().split()))
+N = int(input("Enter number of elements:"))
+num = list(map(int, input().split()))
+print (all(i >= 0 for i in num) and any(str(i) == str(i)[::-1] for i in num))
