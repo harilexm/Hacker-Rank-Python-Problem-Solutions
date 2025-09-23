60330a1ce4b78e4817a96702938b1a33693b3ee8
harilexm <umer.abdullah9891@gmail.com>
2025-09-23 22:45:47 +0500

    code provided

---
+++ Easy/058. Map and Lambda Function.py
@@ -0,0 +1,9 @@
+cube = lambda x:# complete the lambda function
+
+def fibonacci(n):
+    a = 1
+    # return a list of fibonacci numbers
+    
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
