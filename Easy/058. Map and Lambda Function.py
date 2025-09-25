771cec6551af226c634c627d94d2dec49150f165
harilexm <umer.abdullah9891@gmail.com>
2025-09-25 21:45:29 +0500

    Code Completed

--- Easy/058. Map and Lambda Function.py
+++ Easy/058. Map and Lambda Function.py
@@ -2,8 +2,12 @@ cube = lambda x: pow(x, 3) # power of x to 3 times.
 
 def fibonacci(n):
     # return a list of fibonacci numbers
-
-    
+    fib = list()
+    a, b = 0, 1
+    for _ in range(n):
+        fib.append(a)
+        a, b = b, a + b
+        return fib
     
 if __name__ == '__main__':
     n = int(input())
