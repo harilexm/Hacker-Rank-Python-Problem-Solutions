43814374da80f1a83104aef45cb9205c3501f0f9
harilexm <umer.abdullah9891@gmail.com>
2025-09-27 21:35:16 +0500

    Final Code

--- Easy/060. Group(), Groups() & Groupdict().py
+++ Easy/060. Group(), Groups() & Groupdict().py
@@ -1,5 +1,4 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-# Enter your code here. Read input from STDIN. Print output to STDOUT
 import re
 S = input()
 pattern = re.search(r'([a-zA-Z0-9])\1', S)
